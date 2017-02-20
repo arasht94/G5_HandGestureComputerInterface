c74e80fcd93ba53bd985ef64d82cfc1866763f5e
Arash Taheri <arash.taheri.dezfouli@mail.utoronto.ca>
2017-02-20 16:07:48 -0500

    IP code and HDMI Demo Code

---
+++ NexysVideo-master-james/NexysVideo-master/Projects/hdmi/sdk/videodemo/Debug/src/dynclk/dynclk.d
@@ -0,0 +1,25 @@
+src/dynclk/dynclk.o src/dynclk/dynclk.o: ../src/dynclk/dynclk.c \
+ ../src/dynclk/dynclk.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_io.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/mb_interface.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_assert.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_exception.h \
+ ../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_printf.h
+
+../src/dynclk/dynclk.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_io.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/mb_interface.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_exception.h:
+
+../src/dynclk/../../../videodemo_bsp/microblaze_0/include/xil_printf.h:
---
+++ NexysVideo-master-james/NexysVideo-master/Projects/hdmi/sdk/videodemo/Debug/src/video_demo.d
@@ -0,0 +1,136 @@
+src/video_demo.o src/video_demo.o: ../src/video_demo.c \
+ ../src/video_demo.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/video_capture/video_capture.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma_hw.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_io.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/mb_interface.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_assert.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_exception.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_printf.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma_i.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xdebug.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xstatus.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xvtc.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xvtc_hw.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xgpio.h \
+ ../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xgpio_l.h \
+ ../src/video_capture/../intc/intc.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xstatus.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xparameters.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xintc.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_assert.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xparameters.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xstatus.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xintc_l.h \
+ ../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_io.h \
+ ../src/display_ctrl/display_ctrl.h \
+ ../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/display_ctrl/vga_modes.h \
+ ../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xaxivdma.h \
+ ../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xvtc.h \
+ ../src/display_ctrl/../dynclk/dynclk.h \
+ ../src/display_ctrl/../dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/intc/intc.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xuartlite_l.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xil_assert.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xil_io.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xil_cache.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/mb_interface.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xparameters.h \
+ ../src/../../videodemo_bsp/microblaze_0/include/xparameters.h
+
+../src/video_demo.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/video_capture/video_capture.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma_hw.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_io.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/mb_interface.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_exception.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xil_printf.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xaxivdma_i.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xdebug.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xstatus.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xvtc.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xvtc_hw.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xgpio.h:
+
+../src/video_capture/../../../videodemo_bsp/microblaze_0/include/xgpio_l.h:
+
+../src/video_capture/../intc/intc.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xstatus.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xparameters.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xintc.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xparameters.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xstatus.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xintc_l.h:
+
+../src/video_capture/../intc/../../../videodemo_bsp/microblaze_0/include/xil_io.h:
+
+../src/display_ctrl/display_ctrl.h:
+
+../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/display_ctrl/vga_modes.h:
+
+../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xaxivdma.h:
+
+../src/display_ctrl/../../../videodemo_bsp/microblaze_0/include/xvtc.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+../src/display_ctrl/../dynclk/../../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/intc/intc.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xuartlite_l.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xil_io.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xil_cache.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/mb_interface.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xparameters.h:
+
+../src/../../videodemo_bsp/microblaze_0/include/xparameters.h:
