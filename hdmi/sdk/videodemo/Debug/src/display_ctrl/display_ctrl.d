c2dfc0607f2204ca76046d8f31ac9511e6b44de1
nimra99 <armin_999@hotmail.com>
2017-03-09 16:34:32 -0500

    got IP integration to create bitstream

---
+++ hdmi/sdk/videodemo/Debug/src/display_ctrl/display_ctrl.d
@@ -0,0 +1,58 @@
+src/display_ctrl/display_ctrl.o src/display_ctrl/display_ctrl.d: \
+ ../src/display_ctrl/display_ctrl.c ../src/display_ctrl/display_ctrl.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h \
+ ../src/display_ctrl/vga_modes.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma_hw.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/mb_interface.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_assert.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_exception.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_printf.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma_i.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xdebug.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xstatus.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xvtc.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xvtc_hw.h \
+ ../src/display_ctrl/../dynclk/dynclk.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xdebug.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h
+
+../src/display_ctrl/display_ctrl.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h:
+
+../src/display_ctrl/vga_modes.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma_hw.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/mb_interface.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_exception.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_printf.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xaxivdma_i.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xdebug.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xstatus.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xvtc.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xvtc_hw.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xdebug.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h:
---
+++ hdmi/sdk/videodemo/Debug/src/dynclk/dynclk.d
@@ -0,0 +1,25 @@
+src/dynclk/dynclk.o src/dynclk/dynclk.d: ../src/dynclk/dynclk.c \
+ ../src/dynclk/dynclk.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/mb_interface.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_assert.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_exception.h \
+ C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_printf.h
+
+../src/dynclk/dynclk.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_io.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_types.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/mb_interface.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_assert.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_exception.h:
+
+C:/Users/Mohammad/Desktop/ECE532/NexysVideo-master/Projects/hdmi/sdk/videodemo_bsp/microblaze_0/include/xil_printf.h:
