1595bf7ca72978d1b870230eceac7be5dccd5d98
Taheri-Dezfouli Arash <dezfoul2@WIN.ecf.utoronto.ca>
2017-03-21 16:26:35 -0400

    integrated custom ip into entire system

---
+++ src/hdmi.sdk/videodemo/Debug/src/display_ctrl/display_ctrl.d
@@ -0,0 +1,61 @@
+src/display_ctrl/display_ctrl.o src/display_ctrl/display_ctrl.o: \
+ ../src/display_ctrl/display_ctrl.c ../src/display_ctrl/display_ctrl.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../src/display_ctrl/vga_modes.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_hw.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_exception.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_printf.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc_hw.h \
+ ../src/display_ctrl/../dynclk/dynclk.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h \
+ W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h
+
+../src/display_ctrl/display_ctrl.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../src/display_ctrl/vga_modes.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_hw.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_exception.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_printf.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc_hw.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h:
+
+W:/ece532/integration/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h:
---
+++ src/hdmi.sdk/videodemo/Debug/src/video_demo.d
@@ -0,0 +1,95 @@
+src/video_demo.o src/video_demo.o: ../src/video_demo.c \
+ ../src/video_demo.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../src/video_capture/video_capture.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_hw.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_exception.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_printf.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc_hw.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xgpio.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xgpio_l.h \
+ ../src/video_capture/../intc/intc.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xintc.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xintc_l.h \
+ ../src/display_ctrl/display_ctrl.h ../src/display_ctrl/vga_modes.h \
+ ../src/display_ctrl/../dynclk/dynclk.h ../src/intc/intc.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xuartlite_l.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_cache.h \
+ ../src/vdma/vdma.h \
+ W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h
+
+../src/video_demo.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../src/video_capture/video_capture.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_hw.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_exception.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_printf.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xdebug.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xvtc_hw.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xgpio.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xgpio_l.h:
+
+../src/video_capture/../intc/intc.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xintc.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xintc_l.h:
+
+../src/display_ctrl/display_ctrl.h:
+
+../src/display_ctrl/vga_modes.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+../src/intc/intc.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xuartlite_l.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xil_cache.h:
+
+../src/vdma/vdma.h:
+
+W:/ece532/src/hdmi.sdk/standalone_bsp_0/microblaze_0/include/xaxivdma_i.h:
